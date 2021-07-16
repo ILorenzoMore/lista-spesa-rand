9da46230e65dc9ada071afef9ab894ed94737e57
Lorenzo Moré <lrnzmore@gmail.com>
2021-07-16 09:56:31 +0200

    test

---
+++ server/lista-spesa/src/main/java/it/corso/listaspesa/ListaSpesaApplication.java
@@ -0,0 +1,13 @@
+package it.corso.listaspesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaSpesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaSpesaApplication.class, args);
+	}
+
+}
---
+++ server/lista-spesa/src/test/java/it/corso/listaspesa/ListaSpesaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.corso.listaspesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaSpesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
