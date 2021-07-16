a9c929c3318d04bf907d48ee8ac80be908d5e8a6
Peppestra <giuseppestrancia88@gmail.com>
2021-07-16 12:10:27 +0200

    modificato client

--- client/lista-spesa/src/app/app.component.ts
+++ client/lista-spesa/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Prodotto } from './prodotto';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lista-spesa';
+  prodotto: Prodotto = new Prodotto();
+  prodotti: Prodotto[] = [{nome: "pane"}, {nome: "pera"}];
+
+  constructor (private http:HttpClient){
+
+    let ox: Observable<Prodotto[]> = http.get<Prodotto[]>("localhost:8080/listaProdotti")
+    ox.subscribe(u => this.prodotti = u);
+  }
+
+  aggiungi(){
+    console.log('siamo in aggiungi');
+  }
+
+  rimuovi(){
+    console.log('siamo in rimuovi',this.prodotto);
+  }
 }
